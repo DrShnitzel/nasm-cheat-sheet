0d87eac9089847ef090ca5268745a005c3a349b1
Aleksei Zaitsev <drshnitsel@gmail.com>
2021-05-05 17:12:18 +0300

    Add hello world example

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # nasm-cheat-sheet
+
+Code is written for 64bit Linux.
+
+## build
+
+depends on nasm and ld
+
+```sh
+nasm -f elf64 app.asm && ld app.o -o app && ./app
+```
+
+## debug
+
+```sh
+nasm -g -f elf64 app.asm -l app.list && ld app.o -o app && ./app
+gdb -q -x file.gdb
+```
+
---
+++ app.asm
@@ -0,0 +1,32 @@
+global _start	; nasm derective to set entry point for the program
+
+; this section is used to store initilized data
+; you can concatenate
+section .data
+        initialized_msg db "I was defined before program ", "execution", 0xA
+
+; this section is used to store data initialized by zeroes
+; it is separate section because it is cheaper to initialize large sequential
+; amount of data with zeros than scathered data
+section .bss
+        uninitialized_msg resb 13
+
+section .text
+
+_start:
+	; when calling syscall we put syscall number in rax
+	; other arguments are put in registers rdi, rsi, rdx, rcx, r8, r9
+	mov rsi, initialized_msg ; address where message starts
+	mov rdx, 100 ; length of the message
+	call _print
+        call _exit
+
+_print:
+        mov rax, 1 ; write syscall
+        mov rdi, 1 ; file descriptor where to write, 1 = to STDOUT
+        syscall
+
+_exit:
+        mov rax, 60
+        xor rdi, rdi ; put 0 inside rdi, 0 - exit code = all OK
+        syscall
